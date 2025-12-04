9421938cb1d655e1cc28b64acd7298ad2abba1d6
Obg <121403838+OBG001@users.noreply.github.com>
2025-12-04 14:54:01 +0300

    Wew
    For wew

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='obg-utilities', 
+    version='0.1.0',      
+    packages=['obg_source'], 
+    description='A simple utility package containing the O_bg function.',
+    author='OBG',
+    url='https://github.com/OBG001/-'
+)
