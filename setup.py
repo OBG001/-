4b48ca75f75d44614fcff756dfe37aff4adb7a94
Obg <121403838+OBG001@users.noreply.github.com>
2025-12-04 15:50:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import setup, find_packages
 
+try:
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = 'A simple utility package for Discord helper functions.'
+
 setup(
     name='obg-utilities', 
     version='0.1.0',      
-    packages=['obg_source'], 
-    description='A simple utility package containing the O_bg function.',
+    packages=find_packages(), # يستخدم find_packages() لضمان العثور على 'obg_source'
+    description='A Discord utility package for sending messages to verified members.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='OBG',
-    url='https://github.com/OBG001/-'
+    url='https://github.com/OBG001/-',
+    install_requires=[
+        'discord.py>=2.0.0', 
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
