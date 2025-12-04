31f79ce5cdcf8fbb8c5636963ea009b09f5bfd60
Obg <121403838+OBG001@users.noreply.github.com>
2025-12-04 15:42:56 +0300

    Update o_bg.py

--- obg_source/o_bg.py
+++ obg_source/o_bg.py
@@ -1,2 +1,29 @@
-def o_bg():
-   print("Obg is here")
+##############
+# code: send_all_message bot for discord 
+# version: 0.0.1
+# by: obg
+##############
+
+import asyncio
+
+async def send_message_to_verified(guild, msg, tag_bot=None, check_func=lambda m: True):
+
+    sent_ids = set()
+    count = 0
+
+    for member in guild.members:
+        if member.bot:
+            continue
+        if member.id in sent_ids:
+            continue
+        if not check_func(member):
+            continue
+
+        try:
+            await member.send(msg)
+            sent_ids.add(member.id)
+            count += 1
+        except Exception:
+            pass  
+
+    return count
