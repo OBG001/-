e8ca094dfb83534f40dcf49c9c146618bf542b59
Obg <121403838+OBG001@users.noreply.github.com>
2025-12-07 19:26:24 +0300

    Update o_bg.py

--- obg_source/o_bg.py
+++ obg_source/o_bg.py
@@ -4,29 +4,6 @@
 # by: obg
 ##############
 
-import asyncio
-
-async def smad(guild, msg):
-
-    sent_ids = set()
-    count = 0
-
-    for member in guild.members:
-        if member.bot:
-            continue
-        if member.id in sent_ids:
-            continue
-
-        try:
-            await member.send(msg)
-            sent_ids.add(member.id)
-            count += 1
-        except Exception:
-            pass  
-
-    return count
-
-
 
 import asyncio
 import logging
